e4368a54ef255cce4f1c77d2c5ad1855fdba5dd6
Tate Allen <tatea7@yahoo.com>
2017-02-25 14:18:01 -0800

    deez dank commits

--- tate/main.py
+++ tate/main.py
@@ -2,6 +2,7 @@ import sqlite3
 import numpy as np
 import pandas as pd
 from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 import tensorflow as tf
 
 def clean_frame(frame):
@@ -11,11 +12,12 @@ def clean_frame(frame):
         numeric_frame[column][np.isnan(frame[column])] = numeric_frame[column].min()
 
 def normalize_frame(frame):
-    numeric_frame = frame._get_numeric_data()
-    for column in numeric_frame:
-        max_val = numeric_frame[column].max()
-        min_val = numeric_frame[column].min()
-        numeric_frame[column] = (numeric_frame[column] - min_val) / (max_val - min_val)
+    numeric_frame = frame._get_numeric_data()[0:10]
+    for index, column in enumerate(numeric_frame):
+        if index < len(numeric_frame):
+            max_val = numeric_frame[column].max()
+            min_val = numeric_frame[column].min()
+            numeric_frame[column] = (numeric_frame[column] - min_val) / (max_val - min_val)
 
 connection = sqlite3.connect("../wine.db")
 
@@ -31,11 +33,25 @@ white_dataframe = pd.DataFrame(white_rows, columns=list(zip(*white.description))
 clean_frame(red_dataframe)
 clean_frame(white_dataframe)
 normalize_frame(red_dataframe)
-normalize_frame(white_dataframe)
+# normalize_frame(white_dataframe)
 
-red_numeric = red_dataframe._get_numeric_data()
-white_numeric = white_dataframe._get_numeric_data()
-pca = PCA(n_components=4)
-pca.fit(red_numeric.ix[:, 0:11])
-print(red_numeric.ix[:, 0:11])
-print(pca.components_)
+# red_explanatory = red_dataframe._get_numeric_data().ix[:, 0:11]
+# print(red_explanatory.columns)
+# red_response = red_dataframe._get_numeric_data().ix[12]
+# # white_numeric = white_dataframe._get_numeric_data()
+# pca = PCA(n_components=4)
+# pca.fit(red_explanatory)
+#
+#
+# fig, plot = plt.subplots()
+# fig.set_size_inches(50, 50)
+# plt.prism()
+#
+# red_transformed = pca.fit_transform(red_explanatory)
+# print(pca.explained_variance_ratio_)
+# plot.scatter(red_transformed[:, 0], red_transformed[:, 1], c = red_response)
+# plot.set_xticks(())
+# plot.set_yticks(())
+#
+# plt.tight_layout()
+# plt.show()
